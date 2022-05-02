03df7c584adc151ae51cd199395105c67d9eb1ea
unknown <lucioucca@gmail.com>
2022-05-02 18:59:50 -0300

    componente login

--- propiapp/src/App.js
+++ propiapp/src/App.js
@@ -1,32 +1,62 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/login/login';
 import Profile from './components/profile/profile';
 
-function App() {
-  return (
-    <div className="App">
-      <Profile user={{
-        name: 'Juan',
-        username: 'juanito',
-        email: 'adad@gmail.com',
-        photo: 'https://picsum.photos/200/300',
-      }} />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends React.Component { 
+
+  constructor(props) {
+    super(props);
+
+    this.state = {auth: false};
+
+    this.handleLoginChange = this.handleLoginChange.bind(this)
+  };
+
+  handleLoginChange(auth) {
+    console.log('auth', auth);
+    this.setState({auth: auth} );
+
+    console.log('state', this.state.auth);
+  }
+
+  render() {
+    let pantalla;
+
+    if (this.state.auth === false) {
+      pantalla = <Login className="login" onLoginChange={this.handleLoginChange}></Login>;
+    } else {
+      pantalla = <Profile/>
+    }
+    return (
+      <div className="App">
+        {pantalla}
+      
+        {/*<Login className="login" onLoginChange={this.handleLoginChange}></Login>*/}
+  
+        {/*<Profile user={{
+          name: 'Juan',
+          username: 'juanito',
+          email: 'adad@gmail.com',
+          photo: 'https://picsum.photos/200/300',
+        }} />*/}
+        {/*<header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+      </header>*/}
+      </div>
+    );
+  }
+}
--- propiapp/src/components/login/login.js
+++ propiapp/src/components/login/login.js
@@ -6,14 +6,62 @@ export default class Login extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {username: '',
+                      password: '',
+                      // auth: false
+                    };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    handleSubmit(event) {
+        this.login();
+        event.preventDefault();
+    }
+
+    login() {
+        if(this.state.username === 'user' && this.state.password === 'pass') {
+            // this.setState({auth: true});
+            this.props.onLoginChange(true);
+        }
     }
 
     render() {
-        return (
-            <div>
-                <p>login</p>
-            </div>
-        );
+            return (
+                <div className="login-container">
+                    <h1 className="login-title">LogIn</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="login-input">
+                            <div className="input-container">
+                                <label className="input-label">Username</label>
+                                <input className="input-data"
+                                    type="text"
+                                    name="username"
+                                    value={this.state.username}
+                                    onChange={this.handleChange}/>
+                            </div>
+                            <div className="input-container">
+                                <label className="input-label">Password</label>
+                                <input className="input-data"
+                                    type="password"
+                                    name="password"
+                                    value={this.state.password}
+                                    onChange={this.handleChange}/>
+                            </div>
+                            <div className="input-button">
+                                <input type="submit" value="Submit"/>
+                            </div>
+                        </div>
+                        
+                    </form>
+                </div>
+            );        
     }
+
+
 }
